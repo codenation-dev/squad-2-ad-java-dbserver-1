8c12b46a5a157e16ea6d5455b70748e9abe6be9b
henrique_ramires@terceiros.sicredi.net <henrique_ramires@terceiros.sicredi.net>
2019-10-19 17:06:53 -0300

    first commit

---
+++ error-center-api/src/test/java/com/warmachine/errorcenterapi/ErrorCenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warmachine.errorcenterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorCenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
