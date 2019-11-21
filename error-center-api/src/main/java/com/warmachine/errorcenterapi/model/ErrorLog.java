1720576cc895c290f20ca6402d8de47c6ef34f8b
Mariel <coutomariel@gmail.com>
2019-11-20 21:44:42 -0300

    Adição de dependências, config do h2 no properties e model das classes user e logerror

---
+++ error-center-api/src/main/java/com/warmachine/errorcenterapi/model/ErrorLog.java
@@ -0,0 +1,37 @@
+package com.warmachine.errorcenterapi.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(of = "id")
+public class ErrorLog {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@CreatedDate
+	private LocalDateTime createdAt;
+	
+	@ManyToOne
+	private User user;
+}
---
+++ error-center-api/src/main/java/com/warmachine/errorcenterapi/model/User.java
@@ -0,0 +1,37 @@
+package com.warmachine.errorcenterapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(of = "id")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	@Size(max = 50)
+	private String email;
+	@NotNull
+	@Size(max = 50)
+	private String password;
+	@NotNull
+	private String token;
+	
+}
